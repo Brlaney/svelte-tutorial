961baeb0fecf9a71c4ece9cec855a73da3b4962a
brlaney42 <brlaney@outlook.com>
2022-01-05 16:16:56 -0600

    Pushing updates

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,24 +1,25 @@
 <script>
-  // import {onMount, onDestroy} from 'svelte'
   import {FeedbackStore} from '../stores'
   import { fade, scale } from 'svelte/transition'
   import FeedbackItem from './FeedbackItem.svelte'
+  /*
+    import {onMount, onDestroy} from 'svelte'
+    let feedback = []
 
-  // let feedback = []
+    const unsubscribe = FeedbackStore.subscribe((data) => feedback = data)
 
-  // const unsubscribe = FeedbackStore.subscribe((data) => feedback = data)
+    onMount(() => {
+      console.log('mounted')
+    })
 
-  // onMount(() => {
-  //   console.log('mounted')
-  // })
-
-  // onDestroy(() => {
-  //  unsubscribe()
-  // })
+    onDestroy(() => {
+    unsubscribe()
+    })
+  */
 </script>
 
 {#each $FeedbackStore as fb (fb.id)}
-<div in:scale out:fade="{{ duration: 500 }}">
-  <FeedbackItem item={fb} />
-</div>
+  <div in:scale out:fade="{{ duration: 500 }}">
+    <FeedbackItem item={fb} />
+  </div>
 {/each}
--- src/components/FeedbackStats.svelte
+++ src/components/FeedbackStats.svelte
@@ -1,13 +1,14 @@
 <script>
   import {FeedbackStore} from '../stores'
 
-  $: count = $FeedbackStore.length
-  $: average = $FeedbackStore.reduce((a, {rating}) => a + rating, 0) / $FeedbackStore.length
+  $: count = $FeedbackStore.length;
+  $: average = $FeedbackStore.reduce((a, {rating}) => a + rating, 0) / $FeedbackStore.length;
+  $: ave = average.toFixed(2);
 </script>
 
 <div class="feedback-stats">
   <h4>{count} Reviews</h4>
-  <h4>Ratings Average: {average}</h4>
+  <h4>Ratings Average: {ave}</h4>
 </div>
 
 <style>
